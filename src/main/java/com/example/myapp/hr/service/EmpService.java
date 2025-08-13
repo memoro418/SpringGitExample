13f470eafaf8763f4715a24162d9ea83b3740fb5
memoro418 <memoro418@gmail.com>
2025-08-13 14:53:39 +0900

    Initial commit

---
+++ src/main/java/com/example/myapp/hr/service/EmpService.java
@@ -0,0 +1,30 @@
+package com.example.myapp.hr.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.myapp.hr.dao.IEmpRepository;
+
+
+@Service
+public class EmpService implements IEmpService {
+
+	
+	@Autowired
+	IEmpRepository empRepository;
+	
+	@Override
+	public int getEmpCount() {
+		// TODO Auto-generated method stub
+		return empRepository.getEmpCount();
+	}
+
+	@Override
+	public Map<String, Object> getEmpInfo(int empid) {
+		// TODO Auto-generated method stub
+		return empRepository.getEmpInfo(empid);
+	}
+
+}
